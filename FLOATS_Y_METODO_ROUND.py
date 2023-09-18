cf27884447a26413e0ac72e2bc88cfa4025719b7
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 19:40:09 -0600

    Practica 7

---
+++ FLOATS_Y_METODO_ROUND.py
@@ -0,0 +1,3 @@
+Numero = 17.567383292929200234 #Le damos valor como flotante a la variable Numero
+
+print(round(Numero,5)) #Mostramos en pantalla el valor la cual utilizamos el round para decir cuantos decimales debera tomar (5)
