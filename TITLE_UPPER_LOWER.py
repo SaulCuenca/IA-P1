a1a188590bb8130c48ca04ba19da94a95c00e928
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 18:51:42 -0600

    Practica 3

---
+++ TITLE_UPPER_LOWER.py
@@ -0,0 +1,7 @@
+Nombre = "saul armando cuenca martinez" #declaramos un strings en minusculas
+print(Nombre.title()) #Imprimimos en pantalla el nombre y la convertira a titulo (mayuscula la primer letra de cada palabra)
+
+oracion = "Esta Es Una Frase Para Ser Formateada." #Declaramos la frase con mayusculas y minusculas
+
+print(oracion.upper()) #Imprimimos la Oracion en Mayusculas
+print(oracion.lower()) #Imprimimos la Oracion en MINUSCULA
