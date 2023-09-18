396d1230ece6bbcd61a92a9dfcfe1bf84c3e46f4
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 21:19:46 -0600

    Practica 13

---
+++ CONDICIONAL_IF_ELSE.py
@@ -0,0 +1,15 @@
+#Corrige el siguiente condicional if else.
+
+#color = rojo
+
+#else color == rojo
+#Print "El color es rojo."
+#if color != rojo
+#Print "El color no es rojo."
+
+color = "rojo" #Agregamos un string a color
+
+if color == "rojo": #Rectificamos si color en igual a rojo 
+    print("El color es rojo.") #Mostramos en pantalla si se cumple la condicion
+else:
+    print("El color no es rojo.") #Mostramos en pantalla si no se cumple la condicion
