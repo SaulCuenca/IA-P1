ee2fbcc525ab7078f979ed1c96ae1e8be0065157
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 22:38:04 -0600

    Practica 19

---
+++ BUCLE_FOR.py
@@ -0,0 +1,7 @@
+#Crea un bucle for que itere la siguiente tupla y muestre una frase como esta en cada iteración:
+# 'El color es: ' + color + '.'.
+
+colores = ('rojo','azul','verde','amarillo')#Se crea una tupla con colores mencionados 
+
+for x in colores:
+    print("El color es: " + x + ".")#Mustra en pantalla todo los colores
