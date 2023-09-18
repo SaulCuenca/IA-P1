42f864b7f65c9f5eb67f561c7775cb80d3e96b67
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 21:56:37 -0600

    Practica 15

---
+++ MULTIPLES_CONDICCIONES_IF.py
@@ -0,0 +1,20 @@
+#cómo encadenar múltiples condiciones if sin ningún else o elif, solo para que sepas que se puede hacer, así que no hace falta que le dediques mucho tiempo.
+
+Grado = int(input('Introduce en que semestre vas (solo numeros):\n'))
+
+if Grado == 1:
+    print('Vas en primer semestre.\n')
+if Grado == 2:
+    print('Vas en segundo semestre.\n')
+if Grado == 3:
+    print('Vas en tercer semestre.\n')
+if Grado == 4:
+    print('Vas en cuarto semestre.\n')
+if Grado == 5:
+    print('Vas en quinto semestre.\n')
+if Grado == 6:
+    print('Vas en sexto semestre.\n')
+if Grado == 7:
+    print('Vas en septimo semestre.\n')
+if Grado == 8:
+    print('Vas en octavo semestre.\n')
