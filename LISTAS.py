43ddc156f8beb4952329d13012b3681651504a88
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 19:55:20 -0600

    Practica 8

---
+++ LISTAS.py
@@ -0,0 +1,21 @@
+# De la siguiente lista, ¿qué color está en la posición 3?
+
+colores = ["rojo", "azul", "verde", "amarillo", "marron", "lila", "negro", "rosa"]
+print(colores[3]) #Mostramos en pantalla que color se muestra en la posicion #3
+
+#¿En qué posición se encuentra el color 'rojo'? ¿y el 'rosa'?
+#rojo 0
+#rosa 8
+
+
+#Crea una lista que contenga los siguientes valores en las posiciones indicadas.
+#"uno" en la posición 4.
+#"dos" en la posición 1.
+#"tres" en la posición 0.
+#"cuatro" en la posición 3.
+#"cinco" en la posición 2.
+
+
+Lista = ["tres","dos","cinco","cuatro","uno"] #Creamos la lista con las posiciones brindadas
+print(Lista) #Mostramos en pantalla toda nuestra lista
+print(Lista[0])#Mostramos en pantalla el  dato de la lista
