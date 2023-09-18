bdb95e187f45da5d969a9d8e9a468ce2e0e03780
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 19:26:15 -0600

    Practica 5

---
+++ SUMA_RESTA_MULTIPLICACION_DIVISION.py
@@ -0,0 +1,13 @@
+Suma = 20 + 23 + 44 #Sumamos con el operando '+' valores para que nos de un resultado de 87
+print(Suma)
+
+Resta = 20 + 23 - 52 - 78 #Restamos con el operando '-' valores para que nos de un resultado de -87
+print(Resta)
+
+Multiplicacion = 10 * 87 #Multiplicamos con el operando '*' valores para que nos de un resultado de 870
+print(Multiplicacion)
+
+Division = 5000 / 230 / 2 #Dividimos con el operando '/' valores para que nos de un resultado de 10.
+print(Division)
+
+Operacion = 10 / 5 + 15 - 17  #Utilizamos diferentes operativos para que nos de un resultado de 0
