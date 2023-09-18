bebbfde756b9f854ace88a511f63e39509e736fe
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 23:11:22 -0600

    Practica 24

---
+++ CREAR_Y_LLAMAR_FUNCIONES.py
@@ -0,0 +1,11 @@
+#Crea una función que realice una suma. Para ello, tendrás que añadir dos argumentos (numero1 y numero2). 
+#En su interior, especificarás un print() que muestre el resultado de la suma. Deberás hacer tres llamadas 
+#que como resultado de la suma den los valores 30, 50 y 57000. Los números que introduzcas en la llamada pueden 
+#ser los que quieras siempre que coincidan los resultados en el print().
+
+def suma(num1,num2): #Empezamos con un def para que guarde valores 
+    print("El resultado de la suma es: ",num1 + num2) # Despues mostramos en pantalla la suma de ellos
+
+suma(15,15)
+suma(28,22)
+suma(48000,9000)
