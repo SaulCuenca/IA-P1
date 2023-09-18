7f62279d71d713649b0fb994d1889e1840a9d596
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 20:00:41 -0600

    Practica 8

---
+++ LISTAS_POSICIONES_NEGATIVAS.py
@@ -0,0 +1,11 @@
+#Utiliza las posiciones negativas para acceder e imprimir algunos de los colores de esta lista.Acceder a:
+#   'naranja', 'amarillo', 'lila', 'blanco' y 'rojo'.
+
+Colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']#Creamos la lista con los colores mensionados
+
+
+print(Colores[-1]) #Mostramos en pantalla el ultimo color/elemento de la lista, color naranja
+print(Colores[3]) #Mostramos en pantalla color amarillo
+print(Colores[-5]) #Mostramos en pantalla color lila
+print(Colores[-2]) #Mostramos en pantalla el penultimo color/elemento de la lista, color blanco
+print(Colores[0]) #Mostramos en pantalla el color rojo
