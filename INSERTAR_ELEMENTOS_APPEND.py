81653b061e2b335217245f3914079987104d346e
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 20:23:58 -0600

    Practica 8

---
+++ INSERTAR_ELEMENTOS_APPEND.py
@@ -0,0 +1,9 @@
+#Añade los colores 'fuxia' y 'celeste' a la lista.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']#Creamos una nueva lista con los colores antes mencionados
+
+
+colores.append('fuxia') #Agregaremos el color fuxia a la lista
+colores.append('celeste')#Agregaremos el color celeste a la lista
+
+print(colores) #Mostramos la lista en pantalla para ver los cambios antes hechos
