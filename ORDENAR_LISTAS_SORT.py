68db669792a3fbdd7a98222d42d2a286ee1125da
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 20:42:16 -0600

    Practica 9

---
+++ ORDENAR_LISTAS_SORT.py
@@ -0,0 +1,7 @@
+#Ordena la siguiente lista en orden alfabético descendente (de la letra 'z' a la 'a').
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']  #Creamos una nueva lista con los colores antes mencionados
+
+colores.sort(reverse=True) #Ordenamos las palabras con orden alfabetica descendente
+
+print(colores) #Mostramos nuestra lista de forma descendente
