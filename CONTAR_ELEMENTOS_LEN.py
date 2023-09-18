616fac853580c187aaeec9efedbbe4b7d692dade
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 20:49:36 -0600

    Practica 10

---
+++ CONTAR_ELEMENTOS_LEN.py
@@ -0,0 +1,8 @@
+#Contar los elementos de una lista
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'negro', 'violeta', 'gris'] #Creamos una lista con varios colores
+
+print(len(colores)) #Mostramos en pantalla el numero de elementos de la lista 
+
+longitud_colores = len(colores) #Almacenamos en una variable el numero de elementos
+print(longitud_colores) #Mostramos en pantalla el numero de elemntos
