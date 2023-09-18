c6050007d4382d669783cf085c39569dd3634f25
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 18:18:07 -0600

    Practica 2

---
+++ CONCATENAR_STRINGS.py
@@ -0,0 +1,21 @@
+String1 = "Siempre" #declaramos string 1 y 2
+String2 = "Sonrie"
+String3 = String1 + String2 #concatenamos los dos strings con el operando '+' almacenando en una tercer variable
+print(String3)
+
+String1 = String1 + String2 #concatenamos los strings reciclando una variable
+print(String1)
+
+print("Siempre " + String2) #conc
+
+Nombre = "Saul" 
+Apellido1 = "Cuenca" 
+Apellido2 = "Martinez"
+
+Completo = Nombre + " " + Apellido1 + " " + Apellido2 
+print(Completo)
+
+Numero1 = "10"
+Numero2 = "22"
+Numero3 = Numero1 + Numero2
+print(Numero3)
