41d2bbc9f785e41559483bf10e184d8fd7d476b6
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 20:30:15 -0600

    Practica 8

---
+++ ELEMENTOS_DE_LISTA_INSERT.py
@@ -0,0 +1,11 @@
+# Añade a la siguiente lista los colores 'magenta' y 'turquesa' utilizando insert().
+# Tendrás que posicionar 'magenta' en la posición siguiente a la de 'lila' y 'turquesa' en la penúltima posición.
+# Deberás hacer esto utilizando posiciones de lista negativa
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']#Creamos una lista con colores mencionados
+
+
+colores.insert(-4,'magenta') #Agregamos el color magenta en la posicion siguiente de lilacon numeros negativos
+colores.insert(-1,'turquesa') #Agregamos el color turquesa en la penultima posicion con numeros negativos
+
+print(colores) #Mostramos en pantalla nuestra lista para ver los cambios 
