da0e6b4ca59d8c0bff1b88769774af6cd3dcdc32
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 18:40:58 -0600

    Practica 2

---
+++ CONCATENAR_STRINGS.py
@@ -0,0 +1,21 @@
+String1 = "Siempre" #Declaramos la Primera Frase
+String2 = "Sonrie" #Declaramos la Segunda Frase
+String3 = String1 + String2 #Juntamos las dos frases anteriores con el operando '+' haciendo que la guardemos en otra variable 
+print(String3)
+
+String1 = String1 + String2 #Concatenamos las frases anteriores en el primer String 
+print(String1) #Mostramos en pantalla las frases guardadas  
+
+print("Siempre " + String2) #Concatenamos Palabra con String
+
+Nombre = "Saul"        #Le Decalramos Nombre
+Apellido1 = "Cuenca"   #Le Decalramos Nuestro primer Apellido
+Apellido2 = "Martinez" ##Le Decalramos Nuestro segundo Apellido
+
+Completo = Nombre + " " + Apellido1 + " " + Apellido2  #Guardamos el nombre y los dos apellidos en Completo
+print(Completo)#Mostramos en pantalla Completo
+
+Numero1 = "10"#Le damos un valor
+Numero2 = "22"#Le damos un valor
+Numero3 = Numero1 + Numero2 #Concatenamos valores 
+print(Numero3)#Mostramos en pantalla lo anterior 
