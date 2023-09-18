d20fa4e8af4c09a17dd0e9ecc31e733aa8d68dda
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 21:06:09 -0600

    Practica 12

---
+++ DE_TUPLAS_A_LISTAS.py
@@ -0,0 +1,7 @@
+#Convierte la siguiente lista en una tupla y asegúrate que se haya convertido en tupla correctamente
+# imprimiendo en la consola el tipo de elemento que es.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']#creamos uan lista llamada colores
+
+tupla = tuple(colores) #Nuestra lista pasa a ser tupla
+print(type(tupla))#Mostramos en pantalla nuestra tupla para verificar que si cambio
