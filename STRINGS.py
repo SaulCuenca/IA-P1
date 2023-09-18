60e61870811c8d48e682e21741a418956d54afa6
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 18:27:36 -0600

    Delete STRINGS.py

--- STRINGS.py
+++
@@ -1,7 +0,0 @@
-Variable1 = "Saul Armando Comillas Dobles" #Declaramos la Primer Variables con Comillas Dobles
-Variable2 = 'Saul Armando Comillas Simples'#Declaramos la Segunda Variables con Comillas Simples
-
-print(Variable1); print(Variable2) #Imprimimos Ambas Variables Juntas
-
-Oracion = '"print()" se utiliza para imprimir valores en la consola' #Aqui Guardamos una Oracion con Comillas
-print(Oracion) #Imprimimos la Oracion Anterior 
