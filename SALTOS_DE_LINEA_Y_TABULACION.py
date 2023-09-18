ae819bd9104153d0a88de6422e845dc1305f4a3c
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 19:09:17 -0600

    Practica 4

---
+++ SALTOS_DE_LINEA_Y_TABULACION.py
@@ -0,0 +1,5 @@
+print("-Python.\n-JavaScript.\n-Java.\n-PHP.\n-TypeScript.\n-SQL.\n-COBOL.")
+#Imprimimos en pantalla las palabras anteriores con el salto de linea (\n)
+
+print("\t-Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL.")
+#imprimimos en pantalla las palabras anteriores con una tabulación(\t)
