931d8459ab4897c9f635d5f577fa59a4d53141c6
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 19:34:37 -0600

    Practica 6

---
+++ EXPONENTES.py
@@ -0,0 +1,10 @@
+Multiplicacion = 2*2*2*2*2*2*2*2*2*2 #Calculamos utilizando solo (*) hasta que nos de 1024
+print(Multiplicacion) #Mostramos en pantalla la anterior operacion
+
+#Encontre que de igual forma se puede hacer asi, aque al poner el operador de (*) dos veces la toma como potencia
+
+Potencia = 2**10 #Calculamos la decima potencia con el doble asterisco (**) y Guardamos el resultado
+print(Potencia) #Mostramos en pantalla la enterior operacion
+
+
+
