ee715067d4c0d595d7afd12faf70afc159a561a5
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-17 20:59:58 -0600

    Practica 11

---
+++ TUPLAS.py
@@ -0,0 +1,14 @@
+#Imprime la segunda posición de esta tupla.
+
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja')#Se crea tupla parecido como una lista pero con () y agregamos los colores mencionados
+
+
+print(colores[1]) #Mostramos en pantalla con el nombre de la tupla y la posicion del color asi nos mostrara el azul 
+
+#Utiliza los símbolos de suma y resta para obtener el resultado 25 a partir de los elementos 
+#de la siguiente tupla en una variable llamada operacion.
+
+numeros = (10, 1, 5, 11) #Hacemos la nueva tupla
+
+print("la suma es: ", numeros[0] + numeros[2] + numeros[3] - numeros[1]) 
+#Mostramos en pantalla la sumamos y restamos dentro del print osea los valores de la dupla
