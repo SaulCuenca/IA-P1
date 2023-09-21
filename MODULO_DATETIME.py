c57be151494302badb344679f258c26d9bb8edd7
SaulCuenca <126286987+SaulCuenca@users.noreply.github.com>
2023-09-21 13:31:30 -0600

    Practica 35

---
+++ MODULO_DATETIME.py
@@ -0,0 +1,13 @@
+#Aqui utilizaremos las fechas y horas al momento de ejecutarse
+
+import datetime
+
+fechayhora = datetime.datetime.now()
+
+print("\nLa fecha del dia de hoy y hora que es: \n",fechayhora)
+
+#De igual forma podemos usarlo para poner cualquier fecha que querramos
+
+fechayhora = datetime.datetime(2000, 12, 1, 15, 20)
+
+print("\n\n\nFecha y hora que naci: \n",fechayhora, "\n")
